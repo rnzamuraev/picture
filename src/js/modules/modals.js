c613bddb75e1527cc62b57f3e1f68ffe70522ac4
Ruslan <rnzamuraev@gmail.com>
2022-05-12 03:10:24 +0300

    add modals

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -12,7 +12,10 @@ const modals = () => {
   ) {
     const open = document.querySelectorAll(openSelector),
       modal = document.querySelector(modalSelector),
-      close = document.querySelector(closeSelector);
+      close = document.querySelectorAll(closeSelector);
+    // close = document.querySelector(closeSelector);
+    // const prise = document.querySelectorAll(".fixed-gift");
+    // console.log(prise);
 
     open.forEach((item) => {
       item.addEventListener("click", (event) => {
@@ -21,24 +24,34 @@ const modals = () => {
           closeAllModalWindow();
           modal.style.display = "block";
           document.body.style.overflow = "hidden";
-          // document.body.classList.add("modal-open");
           document.body.style.paddingRight = `${scroll}px`;
         }
       });
     });
 
-    close.addEventListener("click", () => {
-      closeAllModalWindow();
-      document.body.style.overflow = "";
-      // document.body.classList.remove("modal-open");
-      document.body.style.paddingRight = `0px`;
+    close.forEach((item) => {
+      item.addEventListener("click", (e) => {
+        closeAllModalWindow();
+        document.body.style.overflow = "";
+        document.body.style.paddingRight = `0px`;
+        // if (e.target && e.target.item === ".fixed-gift") {
+        //   const prise = (document.querySelector(
+        //     ".fixed-gift"
+        //   ).style.display = "none");
+        // }
+      });
     });
+
+    // close.addEventListener("click", () => {
+    //   closeAllModalWindow();
+    //   document.body.style.overflow = "";
+    //   document.body.style.paddingRight = `0px`;
+    // });
 
     modal.addEventListener("click", (event) => {
       if (event.target === modal && modalClose == true) {
         closeAllModalWindow();
         document.body.style.overflow = "";
-        // document.body.classList.remove("modal-open");
         document.body.style.paddingRight = `0px`;
       }
     });
@@ -46,16 +59,42 @@ const modals = () => {
 
   function showModalByTime(selector, time) {
     setTimeout(function () {
-      document.querySelector(selector).style.display =
-        "block";
-      document.body.style.overflow = "hidden";
-      document.body.style.paddingRight = `${scroll}px`;
+      let display;
+
+      document
+        .querySelectorAll("[data-modal]")
+        .forEach((item) => {
+          if (getComputedStyle(item).display !== "none") {
+            display = "block";
+          }
+        });
+
+      if (!display) {
+        document.querySelector(selector).style.display =
+          "block";
+        document.body.style.overflow = "hidden";
+        document.body.style.paddingRight = `${scroll}px`;
+      }
+      // document.querySelector(selector).style.display =
+      //   "block";
+      // document.body.style.overflow = "hidden";
+      // document.body.style.paddingRight = `${scroll}px`;
     }, time);
   }
 
-  bindModal();
+  bindModal(
+    ".button-design",
+    ".popup-design",
+    ".popup-close"
+  );
+  bindModal(
+    ".button-consultation",
+    ".popup-consultation",
+    ".popup-close"
+  );
+  bindModal(".fixed-gift", ".popup-gift", ".popup-close");
 
-  showModalByTime(".popup", 60000);
+  showModalByTime(".popup-consultation", 6000);
 };
 
 export default modals;
